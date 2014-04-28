6348eaa7b8436a934f6e58a65fdce023fbd54b69
laetitiamassa <laetitiamassa@gmail.com>
2014-04-28 17:33:36 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ujumbox::Application.config.secret_key_base = '012fee85bf6145da2787ee2da886b3f51c92102a26fa7c7210de82c01abfe573433ed2222ba2dff91ddaad8baea5ffc9f9e59d70e068796f03dd8b79543ea5d4'
